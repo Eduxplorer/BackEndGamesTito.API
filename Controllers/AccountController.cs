f82e7c502ec0351baa95bd7edb82bffa6d04258c
Eduardo Santos <edu.santossilvadev@gmail.com>
2025-11-13 10:12:36 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,28 @@
+﻿// --- Controllers/AccountController.cs
+
+using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using BackEndGamesTito.API.Models;
+// Adicionar um repositório para gerenciar a lógica de dados
+
+using System.Threading.Tasks;
+
+using static Microsoft.ApplicationInsights.MetricDimensionNames.TelemetryContext;
+
+// --- ADICIONAR ELEMENTOS PARA CRIPTOGRAFIA --- //
+
+using System.Security.Cryptography;
+using System.Text;
+using BCrypt.Net; // Biblioteca BCrypt para hashing de senhas
+
+// Usar o banco de dados
+
+
+namespace BackEndGamesTito.API.Controllers
+{
+    public class AccountController
+    {
+    }
+}
